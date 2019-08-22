5117f0cdd6f6679b2d21b9f92933567e10176e7c
swtzang <swtzang@example.com>
2019-08-22 09:00:39 +0800

    0821

---
+++ get_data.R
@@ -0,0 +1,53 @@
+# https://fderyckel.github.io/financialanalysiswithR/02-getting_data.html
+library(tidyverse)
+library(lubridate)        #to deal with dates
+library(rvest)            # to webscrap financial data 
+
+library(gridExtra)        # to stack the charts
+library(scales)           # again to deal with dates but on the x-axis
+library(bdscale)          # to remove weekends and holidays
+
+
+# setting up the path within the project for easier portability of code
+thePath <- "/home/swtzang/git/special_topics_corporate_finance"
+
+# we make it a function that take as input a vector of tickers
+tickers <- c("GDX", "MAG")
+i=1
+get_eod_data <- function(ticker_vector){
+  tickers <- ticker_vector
+  for (i in 1:length(tickers)){
+    print(tickers[i])
+    data = quantmod::getSymbols(Symbols = tickers[i],
+                                src = "av",
+                                api.key = "U9M001MPWDVDNQHH", 
+                                output.size = "full", 
+                                adjusted = TRUE, 
+                                auto.assign = FALSE,
+                                index.class="POSIXct")
+    colnames(data) = c("Open", "High", "Low", "Close", "Volume", "Adjusted")
+    zoo::write.zoo(data, paste0(thePath, "/stockdata_av/", tickers[i], ".csv"), 
+                   sep = ",", row.names = FALSE)
+    # I am adding this because, we are limited to 5 download per minutes with the free key.
+    Sys.sleep(14)
+  }}
+
+## Just get data using this function from now on ;-)
+get_eod_data(c("GDX", "MAG"))
+#
+tickers <- c("XOM", "SLB", "CANE", "NIB", "GDXJ")
+#tickers <- c("SLB", "CANE", "NIB", "GDXJ")
+get_eod_data(tickers)
+
+# compute returns
+df <- list()
+for (i in tickers){
+df[[i]] <- read_csv(paste0(thePath, "/stockdata_av/", i, ".csv")) %>% 
+           select(Index, Adjusted) %>% rename(!!i := Adjusted)
+}
+# merge all stock prices
+df_all <- df %>% map_df(gather, key=key, value=value, -Index) %>% 
+          spread(key, value)
+#
+na.omit(df_all)
+
